e8c249aa1f97d95138051025a3955d28cabb75f2
mario <mariohar@stud.ntnu.no>
2022-02-09 17:30:07 +0100

    La til tid i main()

--- alarmclock.c
+++ alarmclock.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <time.h>
 
 void print_menu(void)
 {
     char choice;
-    printf("Welcome! What do you wish to do?\n's' (schedule), 'l' (list), 'c' (cancel), 'x' (exit): ");
+
+    printf("Welcome! What do you wish to do?. \n's' (schedule), 'l' (list), 'c' (cancel), 'x' (exit): ");
     scanf("%c", &choice);
 
     while (1)
@@ -17,8 +19,20 @@ void print_menu(void)
     }
 }
 
+void display_current_time()
+{
+}
+
 int main()
 {
-    print_menu();
+    /* print_menu();*/
+    time_t current_time;
+
+    time(&current_time);
+
+    ctime(&current_time);
+
+    printf("%s", ctime(&current_time));
+
     return 0;
 }
