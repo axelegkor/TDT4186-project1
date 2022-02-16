5e30ae42b9503680f7fedd057b6177fa4b79dd09
Axel Legallais-Korsbakken <axelegkor@gmail.com>
2022-02-16 14:25:18 +0100

    some logic for fork, sleep, kill, and exit

--- alarmclock.c
+++ alarmclock.c
@@ -3,35 +3,66 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 int counter = 0; // Holder styr på hvor neste alarm legges til i time_array
-time_t time_array[10] = {0}; // Inneholder alarmene
+//time_t time_array[10] = {0}; // Inneholder alarmene
+//int pid_array[10] = {0};
+static struct {
+    time_t time;
+    pid_t pid;
+} alarm_tuple[10];
+
+void alarm_set(int sleeptime)
+{
+    sleep(sleeptime);
+    printf("RING");
+    exit(0);
+}
 
 void add_timestamps() // Legger til alarm i time_array
 {
-    char input[19];
-    time_t alarm;
-    struct tm time_tm;
-    
-    printf("Schedule alarm at which date and time? ");
-    scanf("%s", &input);
+    if (counter < 10){
+        char input[19];
+        time_t alarm;
+        struct tm time_tm;
+        printf("Schedule alarm at which date and time? ");
+        scanf("%s", &input);
 
-    memset(&time_tm, 0, sizeof(struct tm));
-    strptime(input, "%Y-%b-%d %T", &time_tm);
-    alarm = mktime(&time_tm);
+        memset(&time_tm, 0, sizeof(struct tm));
+        strptime(input, "%Y-%b-%d %T", &time_tm);
+        alarm = mktime(&time_tm);
 
-    time_array[counter] = alarm;
-    counter++;
+        alarm_tuple[counter].time = alarm;
+        
+        pid_t childPID = fork();
+        if (childPID) { //if in parent process
+            alarm_tuple[counter].pid = childPID;
+        } else { //if in child process
+            alarm_set(5);
+        }
+        counter++;
+
+    } else {
+        printf("Too many alarms scheduled. Cancel an alarm before scheduling a new one.");
+    }
+        
     /* evt. print how many sec from now */
 }
 
 void cancel_alarm(int alarm_number) // Fjerner en alarm fra time_array
 {
+    pid_t killpid = alarm_tuple[alarm_number].pid;
     for (int i = alarm_number; i < 9; i++) {
-        time_array[i] = time_array[i + 1];
-        time_array[9] = 0;
+        alarm_tuple[i].time = alarm_tuple[i + 1].time;
+        alarm_tuple[i].pid = alarm_tuple[i + 1].pid;
+        alarm_tuple[9].time = 0;
+        alarm_tuple[9].pid = 0;
     }
     counter--;
+    kill(killpid, SIGKILL);
 }
 
 const char * current_time()
@@ -69,7 +100,7 @@ int main()
         }
         else if (choice == 'l') {
             for (int i = 0; i < counter; i++) {
-                printf("%ld", &time_array[i]);
+                printf("%ld", &alarm_tuple[i].time);
             }
         }
     }
