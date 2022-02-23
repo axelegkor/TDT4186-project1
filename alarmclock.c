37238fc715849e0c77c60003a2c7e2395a4074ff
mario <mariohar@stud.ntnu.no>
2022-02-23 10:14:17 +0100

    Added alarm sound with execl()

--- alarmclock.c
+++ alarmclock.c
@@ -6,9 +6,9 @@
 #include <signal.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <unistd.h>
 
-int counter = 0; // Holder styr på hvor neste alarm legges til i time_array
-//time_t time_array[10] = {0}; // Inneholder alarmene
+int counter = 0; // Keeps track of where the next alarm should be placed
 static struct {
     time_t time;
     pid_t pid;
@@ -16,7 +16,11 @@ static struct {
 
 void alarm_set(int sleeptime){
     sleep(sleeptime);
-    printf("RING");
+
+    // Playing alarm sound for MacOS
+    execl("/usr/bin/afplay", "/usr/bin/afplay", "/Applications/OpSys/os1/tone.wav");
+
+    printf("RING\n");
     exit(0);
 }
 
@@ -28,7 +32,7 @@ void add_timestamps() // Legger til alarm i time_array
     
     printf("Schedule alarm at which date and time? ");
     fflush(stdin);
-    scanf("%20[^", &input);
+    scanf("%19[^", &input);
 
     // memset(&time_tm, 0, sizeof(struct tm)); // Trengs denne linjen??
     strptime(input, "%Y-%m-%d %H:%M:%S", &time_tm);
@@ -36,8 +40,7 @@ void add_timestamps() // Legger til alarm i time_array
 
     long sec = alarm - time(NULL);
 
-    //printf("There are: %ld seconds until %s\n", sec, input);
-    // printf("sec: %ld", sec);
+    printf("There are: %ld seconds until %s\n", sec, input);
 
     alarm_tuple[counter].time = alarm;
 
@@ -45,13 +48,12 @@ void add_timestamps() // Legger til alarm i time_array
     if (childPID) {
         alarm_tuple[counter].pid = childPID;
     } else {
-        alarm_set(20);
+        alarm_set(sec);
     }
-
     counter++;
 }
 
-void cancel_alarm(int alarm_number) // Fjerner en alarm fra time_array
+void cancel_alarm(int alarm_number)
 {   
     pid_t killpid = alarm_tuple[alarm_number].pid;
     for (int i = alarm_number; i < 9; i++) {
@@ -74,6 +76,15 @@ const char * current_time()
     return "%s", asctime(timeinfo);
 }
 
+void list_alarms() 
+{
+    for (int i = 0; i < counter; i++) {
+        char out[19];
+        strftime(out, 19, "%Y-%m-%d %H:%M:%S", localtime(&alarm_tuple[i].time));
+        printf("Alarm %d is at %s\n", i, out);
+    }
+}
+
 int main()
 {
     char choice;
@@ -89,6 +100,7 @@ int main()
             break;
         }
         else if (choice == 's') {
+            fflush(stdin);
             add_timestamps();
         }
         else if (choice == 'c') {
@@ -98,28 +110,8 @@ int main()
             cancel_alarm(cancel__num);
         }
         else if (choice == 'l') {
-            for (int i = 0; i < counter; i++) {
-                //printf("%ld", &time_array[i]);
-                char out[19];
-                strftime( out, 19, "%Y-%m-%d %H:%M:%S", localtime (&alarm_tuple[i].time));
-                printf("%s\n", out);
-            }
+            list_alarms();
         }
-    }
-    /*time_t alarm;
-    struct tm time_tm;
-
-    printf("skriv noe: ");
-    scanf("%19[^\n", alarm);
-
-    strptime(alarm, "%Y-%m-%d %H:%M:%S", &time_tm);
-
-    time_t unix_time = mktime(&time_tm);
-    long sec = unix_time - time(NULL);
-
-    printf("det er: %ld", sec);*/
-    
-    //add_timestamps();
-
+    } 
     return 0;
 }
